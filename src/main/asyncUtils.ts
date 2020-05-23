828987e0fc369d086157c193e3ae47e8bb447f02
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-05-23 17:06:17 +0200

    Add test

--- src/main/asyncUtils.ts
+++ src/main/asyncUtils.ts
@@ -1,11 +1,11 @@
-async function sleep(s: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, s * 1000))
+async function sleep(timeInMilliseconds: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, timeInMilliseconds))
 }
 
-export async function tryFunction<T>(func: () => Promise<T>, maxNumOfTries = 10): Promise<T> {
+export async function tryFunction<T>(func: () => Promise<T>, maxNumOfTries = 10, sleepFactor = 1000): Promise<T> {
     let error
     for (let i = 0; i < maxNumOfTries; i++) {
-        await sleep(i)
+        await sleep(i * sleepFactor)
         try {
             return await func()
         } catch (err) {
