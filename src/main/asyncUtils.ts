5ae89050a25d6ee2fe33c18707caeda9f65b9785
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-05-23 10:01:42 +0200

    Add linter

--- src/main/asyncUtils.ts
+++ src/main/asyncUtils.ts
@@ -1,10 +1,10 @@
-function sleep(s: number): Promise<void> {
+async function sleep(s: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, s * 1000))
 }
 
 export async function tryFunction<T>(func: () => Promise<T>, maxNumOfTries = 10): Promise<T> {
     let error
-    for(let i = 0; i < maxNumOfTries; i++) {
+    for (let i = 0; i < maxNumOfTries; i++) {
         await sleep(i)
         try {
             return await func()
--- src/main/index.ts
+++ src/main/index.ts
@@ -3,9 +3,9 @@ import { Json } from "./jsonType"
 import { tryFunction } from "./asyncUtils"
 
 export class MqService {
-    private constructor(private channel: amqp.Channel) {}
+    private constructor(private readonly channel: amqp.Channel) {}
 
-    public addListener(queueName: string, listener: (data: string) => Promise<boolean>) {
+    public addListener(queueName: string, listener: (data: string) => Promise<boolean>): void {
         this.channel.assertQueue(queueName)
         this.channel.consume(queueName, async (message) => {
             if (message !== null) {
@@ -17,31 +17,31 @@ export class MqService {
         })
     }
 
-    public sendMessage(queueName: string, data: Json) {
-        const stringifiedData = typeof data === `string` ? data : JSON.stringify(data)
+    public sendMessage(queueName: string, data: Json): void {
+        const stringifiedData = typeof data === "string" ? data : JSON.stringify(data)
         this.channel.assertQueue(queueName)
         this.channel.sendToQueue(queueName, Buffer.from(stringifiedData))
     }
 
-    public close(): Promise<void> {
+    public async close(): Promise<void> {
         return new Promise((resolve, reject) => {
-            this.channel.close(error => error ? reject(error): resolve())
+            this.channel.close(error => !!error ? reject(error): resolve())
         })
     }
 
-    public static connectOnce(url: string): Promise<MqService> {
+    public static async connectOnce(url: string): Promise<MqService> {
         return new Promise((resolve, reject) => {
             amqp.connect(url, (error, connection) => {
-                if(error) reject(error)
+                if (!!error) reject(error)
                 else connection.createChannel((err, channel) => {
-                    if (err) reject(err)
+                    if (!!err) reject(err)
                     else resolve(new MqService(channel))
                 })
             })
         })
     }
 
-    public static connect(url: string, maxNumOfTries = 10): Promise<MqService> {
-        return tryFunction(() => this.connectOnce(url), maxNumOfTries)
+    public static async connect(url: string, maxNumOfTries = 10): Promise<MqService> {
+        return tryFunction(async () => this.connectOnce(url), maxNumOfTries)
     }
 }
